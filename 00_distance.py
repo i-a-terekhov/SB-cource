d7b86f6c365b534112105864bc7809a01dc68336
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-20 18:50:40 +0400

    feat(00_distance): calculate the distance between cities
    lesson_002, task 00

--- 00_distance.py
+++ 00_distance.py
@@ -3,7 +3,7 @@
 
 # Есть словарь координат городов
 
-sites = {
+cites = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
@@ -14,7 +14,17 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+def distances_calc(a_tuple, b_tuple):
+    exact_distance = ((a_tuple[0] - b_tuple[0]) ** 2 + (a_tuple[1] - b_tuple[1]) ** 2) ** 0.5
+    return round(exact_distance, 1)
+
+distances['Moscow'], distances['London'], distances['Paris'] = {}, {}, {}
+distances['Moscow']['London'] = distances_calc(cites['Moscow'], cites['London'])
+distances['Moscow']['Paris'] = distances_calc(cites['Moscow'], cites['Paris'])
+distances['London']['Moscow'] = distances_calc(cites['London'], cites['Moscow'])
+distances['London']['Paris'] = distances_calc(cites['London'], cites['Paris'])
+distances['Paris']['Moscow'] = distances_calc(cites['Paris'], cites['Moscow'])
+distances['Paris']['London'] = distances_calc(cites['Paris'], cites['London'])
 
 print(distances)
 
