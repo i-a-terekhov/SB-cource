dcbbf4622ed560bd4b56ec2babe2d4d72cbe1474
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-25 03:03:37 +0400

    feat(07_secret): decipher the text with slices
    lesson_002, task 07

--- 07_secret.py
+++ 07_secret.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # есть зашифрованное сообщение
 
 secret_message = [
@@ -21,4 +18,8 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+print(secret_message[0][3])
+print(secret_message[1][9:13])
+print(secret_message[2][5:15:2])
+print(secret_message[3][12:6:-1])
+print(secret_message[4][20:15:-1])
