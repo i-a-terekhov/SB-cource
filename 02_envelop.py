521e8b38e459f4c307921ee57f1084088c23effc
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-26 19:40:36 +0400

    feat(02_envelop): put the bricks in the hole
    lesson_003, task 02

--- 02_envelop.py
+++ 02_envelop.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # (if/elif/else)
 
 # Заданы размеры envelop_x, envelop_y - размеры конверта и paper_x, paper_y листа бумаги
@@ -10,17 +8,24 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
-# проверить для
-# paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
-# paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
-# (просто раскоментировать нужную строку и проверить свой код)
+paper_book = [
+    (8, 9),
+    (9, 8),
+    (6, 8),
+    (8, 6),
+    (3, 4),
+    (11, 9),
+    (9, 11),
+]
+for paper in paper_book:
+    paper_x, paper_y = paper
+    answer = ''
+    if envelop_x >= paper_x and envelop_y >= paper_y:
+        answer = 'ДА'
+    else:
+        answer = 'НЕТ'
+    print(f'Поместится ли в конверте [{envelop_x}, {envelop_y}] лист бумаги [{paper_x}, {paper_y}]?', answer)
 
-# TODO здесь ваш код
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,25 +33,32 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
-# (просто раскоментировать нужную строку и проверить свой код)
-
-# TODO здесь ваш код
+hole_x, hole_y = 8, 9
+brick_car = [
+    (11, 10, 2),
+    (11, 2, 10),
+    (10, 11, 2),
+    (10, 2, 11),
+    (2, 10, 11),
+    (2, 11, 10),
+    (3, 5, 6),
+    (3, 6, 5),
+    (6, 3, 5),
+    (6, 5, 3),
+    (5, 6, 3),
+    (5, 3, 6),
+    (11, 3, 6),
+    (11, 6, 3),
+    (6, 11, 3),
+    (6, 3, 11),
+    (3, 6, 11),
+    (3, 11, 6),
+]
+for brick in brick_car:
+    brick_x, brick_y, brick_z = brick
+    answer = ''
+    if hole_x >= brick_x and hole_y >= brick_y:
+        answer = 'ДА'
+    else:
+        answer = 'НЕТ'
+    print(f'Поместится ли в дыре [{hole_x}, {hole_y}] кирпич [{brick_x}, {brick_y}, {brick_z}]?', answer)
