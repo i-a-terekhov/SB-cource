80151cebd5de11813152d07275731e045f9b9d79
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-27 06:48:08 +0400

    feat(07_wall): build a wall of bricks
    lesson_003, task 07

--- 07_wall.py
+++ 07_wall.py
@@ -1,11 +1,25 @@
-# -*- coding: utf-8 -*-
-
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+brick_len = 70
+brick_hei = 30
 
-simple_draw.pause()
+point_x0 = 2
+point_y0 = 2 + brick_hei
+global_point_x = point_x0
+for k in range(12):
+    for j in range(6):
+        direct_len = [1, 1, 0, 0]
+        direct_hei = [0, -1, -1, 0]
+        start_point_for_brick = sd.get_point(point_x0, point_y0)
+        for i in range(4):
+            end_point_for_brick = sd.get_point(point_x0 + brick_len * direct_len[i], point_y0 + brick_hei * direct_hei[i])
+            sd.line(start_point=start_point_for_brick, end_point=end_point_for_brick, color=sd.COLOR_DARK_RED, width=5)
+            start_point_for_brick = end_point_for_brick
+        point_x0 += brick_len
+    point_y0 += brick_hei
+    point_x0 = global_point_x + (brick_len / 2) * ((k + 1) % 2)
+sd.pause()
