9518c53755b05e6606eb7e52f47b864063f4ce5a
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-28 13:28:18 +0400

    feat(practice 02_snowflake): draw one falling snowflake
    lesson_004, practice_02

--- practice/02_snowflake.py
+++ practice/02_snowflake.py
@@ -1,41 +1,26 @@
-# -*- coding: utf-8 -*-
-
 import simple_draw as sd
+import random
 
 sd.resolution = (1200, 600)
-# познакомится с параметрами библиотечной функции рисования снежинки sd.snowflake()
-
-# sd.snowflake(center=point_0, length=200, factor_a=0.5)
 
 # реализовать падение одной снежинки
+# реализовать падение одной снежинки из произвольного места экрана
+# реализовать падение одной снежинки с ветром - смещение в сторону
 y = 500
 x = 100
 
-y2 = 450
-x2 = 150
 while True:
     sd.clear_screen()
     point = sd.get_point(x, y)
-    sd.snowflake(center=point, length=50)
-    y -= 10
+    sd.snowflake(center=point, length=150, factor_a=0.5, factor_b=0.3, factor_c=60)
+    sd.snowflake(center=point, length=100, factor_a=0.5, factor_b=0.2, factor_c=60)
+    y -= 1 + random.random() * 3
     if y < 50:
        break
-    x = x + 10
+    x = x + 3 + (-5 + random.random() * 5)
 
-    point2 = sd.get_point(x2, y2)
-    sd.snowflake(center=point2, length=30)
-    y2 -= 10
-    if y2 < 50:
-       break
-    x2 = x2 + 20
-
-    sd.sleep(0.1)
+    sd.sleep(0.06)
     if sd.user_want_exit():
         break
-
-# реализовать падение одной снежинки из произвольного места экрана
-
-# реализовать падение одной снежинки с ветром - смещение в сторону
-
 
 sd.pause()
