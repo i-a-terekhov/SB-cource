8f357d3539ea40858048538a6cce385187bdc865
Ivan Terekhov <for-spam32@yandex.ru>
2023-02-28 06:36:06 +0400

    feat(practice 01_triangle): draw a triangle
    lesson_004, practice_01

--- practice/01_triangle.py
+++ practice/01_triangle.py
@@ -1,38 +1,25 @@
-# -*- coding: utf-8 -*-
-
-# pip install simple_draw
-
+# нарисовать треугольник из точки (300, 300) с длиной стороны 200
+# определить функцию рисования треугольника из заданной точки с заданным наклоном
 import simple_draw as sd
 
-# нарисовать треугольник из точки (300, 300) с длиной стороны 200
-length = 200
-point = sd.get_point(300, 300)
 
-# v1 = sd.get_vector(start_point=point, angle=0, length=200, width=3)
-# v1.draw()
-#
-# v2 = sd.get_vector(start_point=v1.end_point, angle=120, length=200, width=3)
-# v2.draw()
-#
-# v3 = sd.get_vector(start_point=v2.end_point, angle=240, length=200, width=3)
-# v3.draw()
-
-# определить функцию рисования треугольника из заданной точки с заданным наклоном
-def triangle(point, angle=0):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=200, width=3)
-    v1.draw()
-
-    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=200, width=3)
-    v2.draw()
-
-    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 240, length=200, width=3)
-    v3.draw()
+def figure(point, angle=0, length=200, width=3):
+    quan_corners = 3
+    local_point = point
+    for i in range(quan_corners):
+        side = sd.get_vector(
+            start_point=local_point,
+            angle=angle + i * (360 / quan_corners),
+            length=length,
+            width=width
+        )
+        side.draw()
+        local_point = side.end_point
 
 
-point_0 = sd.get_point(300, 300)
-
-for angle in range(0, 361, 30):
-    triangle(point=point_0, angle=angle)
+for angl in range(0, 361, 15):
+    start_point = sd.get_point(50 + angl, 50 + angl)
+    figure(point=start_point, angle=angl, length=100, width=1)
 
 sd.pause()
 
