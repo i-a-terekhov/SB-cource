2b18ff80fd6ef3088f692fed74a66238ebda6721
Ivan Terekhov <for-spam32@yandex.ru>
2023-03-03 18:41:37 +0400

    feat(01_inhabitants): print the values of the variables from the import
    lesson_005, task 01

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
-
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+import room_1 as apart_1
+import room_2 as apart_2
 
+
+for room_number in [apart_1, apart_2]:
+    print(f'В комнате {room_number.__name__} живут:', room_number.folks)
 
