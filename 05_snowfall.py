4f02125dd251120b38870bc60d87b8f0816e8ef9
Ivan Terekhov <for-spam32@yandex.ru>
2023-03-03 14:55:48 +0400

    feat(05_snowfall): make a snowfall with a growing snowdrift
    lesson_004, task 05

--- 05_snowfall.py
+++ 05_snowfall.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-
-import simple_draw as sd
-
-
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
-
-N = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,13 +10,66 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+import simple_draw as sd
+import random
+
+
+def generate_snowflakes(number_of_it):
+    snowflakes = [{
+        'Ox': round(0 + random.random() * 1200),
+        'Oy': round(600 + random.random() * 600),
+        'length': round(20 + random.random() * 5),
+        'factor_a': round(0.4 + random.random() * 0.1, 1),
+        'factor_b': round(0.4 + random.random() * 0.1, 1),
+        'factor_c': round(35 + random.random() * 10),
+    } for i in range(number_of_it)]
+    return snowflakes
+
+
+sd.resolution = (1200, 600)
+snowflakes_dict = generate_snowflakes(40)
+
+total_frames = 0
 while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
+    total_frames += 1
+
+    sd.start_drawing()
+    for flake in range(len(snowflakes_dict)):
+
+        if snowflakes_dict[flake]['Oy'] < round(total_frames / 100):
+            snowflakes_dict[flake] = generate_snowflakes(1)[0]
+            print(round(total_frames / 100), snowflakes_dict[flake])
+            continue
+
+        # рисуем снежинки цветом фона
+        sd.snowflake(
+            center=sd.get_point(snowflakes_dict[flake]['Ox'], snowflakes_dict[flake]['Oy']),
+            length=snowflakes_dict[flake]['length'],
+            color=sd.background_color,
+            factor_a=snowflakes_dict[flake]['factor_a'],
+            factor_b=snowflakes_dict[flake]['factor_b'],
+            factor_c=snowflakes_dict[flake]['factor_c'],
+        )
+
+        # считаем координаты следующего местоположения снежинок
+        snowflakes_dict[flake]['Oy'] -= int(random.random() * 5)
+        if round((total_frames / 50)) % 2 == 0:
+            snowflakes_dict[flake]['Ox'] += int(random.random() * 6)
+        elif round((total_frames / 50)) % 2 != 0:
+            snowflakes_dict[flake]['Ox'] -= int(random.random() * 6)
+
+        # рисуем снежинки белым цветом
+        sd.snowflake(
+            center=sd.get_point(snowflakes_dict[flake]['Ox'], snowflakes_dict[flake]['Oy']),
+            length=snowflakes_dict[flake]['length'],
+            color=sd.COLOR_WHITE,
+            factor_a=snowflakes_dict[flake]['factor_a'],
+            factor_b=snowflakes_dict[flake]['factor_b'],
+            factor_c=snowflakes_dict[flake]['factor_c'],
+        )
+
+    sd.finish_drawing()
+    sd.sleep(0.005)
     if sd.user_want_exit():
         break
 
