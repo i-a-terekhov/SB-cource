88789c9066cb249ca8e85a18042d16d24453fc01
Ivan Terekhov <for-spam32@yandex.ru>
2023-03-04 18:51:46 +0400

    feat(03_burger): prepare a burger from the plug-ints
    added a module with the functions of adding ingredients
    
    lesson_005, task 03

--- 03_burger.py
+++ 03_burger.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Создать модуль my_burger. В нем определить функции добавления инградиентов:
+# Создать модуль my_burger. В нем определить функции добавления ингредиентов:
 #  - булочки
 #  - котлеты
 #  - огурчика
@@ -15,4 +13,17 @@
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
 
-# TODO здесь ваш код
+import my_burger
+
+
+def twince_burger():
+    my_burger.add_buns()
+    my_burger.add_cucumber()
+    my_burger.add_mayonnaise()
+    my_burger.add_cheese()
+    my_burger.add_tomato()
+    my_burger.add_cotlets()
+    print('Бутрик готов!')
+
+
+twince_burger()
