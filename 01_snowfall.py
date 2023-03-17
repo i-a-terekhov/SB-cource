b71357732697dcaef1511f618640d2cfb38a60e4
Ivan Terekhov <for-spam32@yandex.ru>
2023-03-17 21:30:53 +0400

    feat(01_snowfall): create a snowflake class with the behavior
    lesson_007, task 01

--- 01_snowfall.py
+++ 01_snowfall.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-
 import simple_draw as sd
+import random
+
 
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
@@ -9,9 +9,44 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
 
-    # TODO здесь ваш код
+    def __init__(self):
+        self.resolution = sd.resolution
+        self.param = {
+            'Ox': round(random.random() * self.resolution[0]),
+            'Oy': round(self.resolution[1] + random.random() * self.resolution[1] * 0.2),
+            'length': round(20 + random.random() * 5),
+            'color': None,
+            'factor_a': round(0.4 + random.random() * 0.1, 1),
+            'factor_b': round(0.4 + random.random() * 0.1, 1),
+            'factor_c': round(35 + random.random() * 10),
+        }
+
+    def clear_previous_picture(self):
+        background_color = sd.background_color
+        self.draw(background_color)
+
+    def move(self):
+        self.param['Oy'] -= int(random.random() * 15)
+        self.param['Ox'] -= int(random.random() * -10 + 5)
+
+    def draw(self, color=None):
+        if color is None:
+            color = sd.COLOR_WHITE
+        sd.snowflake(
+            center=sd.get_point(self.param['Ox'], self.param['Oy']),
+            length=self.param['length'],
+            color=color,
+            factor_a=self.param['factor_a'],
+            factor_b=self.param['factor_b'],
+            factor_c=self.param['factor_c'],
+        )
+
+    def can_fall(self):
+        if self.param['Oy'] > 10:
+            return True
+        else:
+            return False
 
 
 flake = Snowflake()
