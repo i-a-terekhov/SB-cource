a2f7304cc3736e4b65567968d9f9f55ad4fdb8da
Ivan Terekhov <for-spam32@yandex.ru>
2023-04-20 13:43:23 +0400

    feat(01_fifth_element): handle exceptional situations with a function
    lesson_010, task_01

--- 01_fifth_element.py
+++ 01_fifth_element.py
@@ -2,12 +2,26 @@
 
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
+def ExceptKipper(exception):
+    if exception == IndexError:
+        print(f'Получили ошибку {str(type(exception)).split()[1][:-1]}: {exception}')
+        print('Количество символов недостаточно')
+    elif exception == ValueError:
+        print(f'Получили ошибку {str(type(exception)).split()[1][:-1]}: {exception}')
+        print('Получен символ, ожидалось число')
+    elif exception == BaseException:
+        print(f'Получили ошибку {str(type(exception)).split()[1][:-1]}: {exception}')
+
+
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except BaseException:
+    ExceptKipper(exception=BaseException)
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
