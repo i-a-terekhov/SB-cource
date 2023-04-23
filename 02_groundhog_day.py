f4744543348766aef343736e9e2d4a46f6c8cb69
Ivan Terekhov <for-spam32@yandex.ru>
2023-04-23 18:35:19 +0400

    refactor(02_groundhog_day): refactor to the class ExceptLogger
    lesson_010, task_02

--- 02_groundhog_day.py
+++ 02_groundhog_day.py
@@ -18,6 +18,7 @@
 # базовых встроенных исключений.
 
 import random
+from logging import exception
 
 
 class IamGodError(Exception):
@@ -56,6 +57,39 @@ class SuicideError(Exception):
         return 'Суицидальность'
 
 
+class ExceptLogger:
+
+    def __init__(self, day=None, exception=None):
+        if day is None or exception is None:
+            print('Некорректные данные на входе в __init__')
+            raise BaseException
+        else:
+            self.except_processor_answer = self.except_processor(exception=exception)
+            self.log_update(day=day, exception=exception, answer=self.except_processor_answer)
+
+    def except_processor(self, exception):
+        if isinstance(exception, IamGodError):
+            return "Oh no, someone thinks they're God!"
+        elif isinstance(exception, DrunkError):
+            return "Looks like someone's had too much to drink..."
+        elif isinstance(exception, CarCrashError):
+            return "There's been a car crash, are you alright?"
+        elif isinstance(exception, GluttonyError):
+            return "Slow down there, you're going to make yourself sick!"
+        elif isinstance(exception, DepressionError):
+            return "It's okay to not be okay, let's talk about it."
+        elif isinstance(exception, SuicideError):
+            return "Please don't do anything drastic, there is always help available."
+        else:
+            return "Unknown cause...", exception
+
+    def log_update(self, day, exception, answer):
+        with open('Log_of_carmas.txt', 'a') as log:
+            log.write(
+                f'Day {str(day):3} - Except: {exception.__str__():15} - Processor answer: {answer} \n'
+            )
+
+
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
 
@@ -65,32 +99,6 @@ def one_day():
         which_error = random.randint(0, len(errors) - 1)
         raise errors[which_error]
     return random.randint(1, 7)
-
-
-def except_processor(exception):
-
-    if isinstance(exception, IamGodError):
-        return "Oh no, someone thinks they're God!"
-    elif isinstance(exception, DrunkError):
-        return "Looks like someone's had too much to drink..."
-    elif isinstance(exception, CarCrashError):
-        return "There's been a car crash, are you alright?"
-    elif isinstance(exception, GluttonyError):
-        return "Slow down there, you're going to make yourself sick!"
-    elif isinstance(exception, DepressionError):
-        return "It's okay to not be okay, let's talk about it."
-    elif isinstance(exception, SuicideError):
-        return "Please don't do anything drastic, there is always help available."
-    else:
-        return "Unknown cause...", exception
-
-
-def log_update(day, exception, answer):
-    with open('Log_of_carmas.txt', 'a') as log:
-        log.write(
-            f'Day {str(day):3} - Except: {exception.__str__():15} - Processor answer: {answer}'
-        )
-        log.write('\n')
 
 
 day = 0
@@ -104,8 +112,7 @@ while current_carma < ENLIGHTENMENT_CARMA_LEVEL:
         print(f'День {day}, сегодняшняя карма равна {carma_today}, всего уровень кармы достиг отметки {current_carma}')
     except BaseException as e:
         print(f'День {day}, сегодня карма не повысилась, уровень кармы остался на отметке {current_carma}')
-        except_processor_answer = except_processor(exception=e)
-        log_update(day=day, exception=e, answer=except_processor_answer)
+        except_instance = ExceptLogger(day, e)
 
 
 # https://goo.gl/JnsDqu
