9381cdda9d12dff93bf8efacf95845b2ce64edf2
Ivan Terekhov <for-spam32@yandex.ru>
2023-04-30 23:13:26 +0400

    feat(04_error_decorator): make a decorator that will log errors
    lesson_011, task_04

--- 04_error_log_decorator.py
+++ 04_error_log_decorator.py
@@ -7,19 +7,33 @@
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
+import time
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+
+def log_errors(log_file_name):
+    def log_file_choser(func):
+        def log_writter(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                with open(log_file_name, 'a') as log:
+                    log.write(
+                        f'{time.strftime("%x %X", time.gmtime())} An error occurred in function {func.__name__}: {str(e)}\n')
+                raise
+            return result
+
+        return log_writter
+
+    return log_file_choser
 
 
 # Проверить работу на следующих функциях
-@log_errors
+@log_errors(log_file_name='log_file_for_perky.txt')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors(log_file_name='log_file_for_check_line.txt')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -43,8 +57,11 @@ for line in lines:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
 
+try:
+    perky(param=42)
+except Exception as exc:
+    print(f'Invalid format: {exc}')
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
@@ -52,4 +69,3 @@ perky(param=42)
 # @log_errors('function_errors.log')
 # def func():
 #     pass
-
