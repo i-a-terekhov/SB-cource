248b35ef6272609c1a6d62cd63ce6bb2b39130df
Ivan Terekhov <for-spam32@yandex.ru>
2023-08-02 19:07:11 +0400

    feat: make a function that adds data to the ticket
    lesson_013

--- 01_ticket.py
+++ 01_ticket.py
@@ -8,9 +8,47 @@
 # Пример заполнения lesson_013/images/ticket_sample.png
 # Подходящий шрифт искать на сайте ofont.ru
 
+from PIL import Image, ImageDraw, ImageFont, ImageColor
+import os
+
+
+SCALING = 1.1
+
+
 def make_ticket(fio, from_, to, date):
-    # TODO здесь ваш код
-    pass
+    pic_of_ticker = Image.open("images\\ticket_template.png")
+    w, h = pic_of_ticker.size
+    pic_of_ticker = pic_of_ticker.resize((int(w * SCALING), int(h * SCALING)))
+
+    font_path = os.path.join("fonts", "ofont.ru_Manrope.ttf")
+    draw = ImageDraw.Draw(pic_of_ticker)
+    font = ImageFont.truetype(font_path, size=26)
+
+    w, h = pic_of_ticker.size
+    percent_h_name = 0.28
+    percent_h_from = 0.45
+    percent_h_to = 0.62
+    percent_h_date = 0.62
+    percent_w_first_column = 0.07
+    percent_w_second_column = 0.43
+
+    message = f"ФИО ПАССАЖИРА"
+    draw.text((w * percent_w_first_column, h * percent_h_name), message, font=font, fill=ImageColor.colormap['black'])
+
+    message = f"ОТКУДА"
+    draw.text((w * percent_w_first_column, h * percent_h_from), message, font=font, fill=ImageColor.colormap['black'])
+
+    message = f"КУДА"
+    draw.text((w * percent_w_first_column, h * percent_h_to), message, font=font, fill=ImageColor.colormap['black'])
+
+    message = f"ДАТА"
+    draw.text((w * percent_w_second_column, h * percent_h_date), message, font=font, fill=ImageColor.colormap['black'])
+
+
+    pic_of_ticker.save("images\\ticket_sample_result.png")
+
+
+make_ticket(fio="Terekhov I.A.", from_="NOVOSIBIRSK", to="BATUMI", date="01.04.23")
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля agrparse.
